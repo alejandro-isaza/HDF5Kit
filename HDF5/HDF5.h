865e9e04b98cbb53b4a268677b6d0ab38c46b75b
Alejandro Isaza <aisaza@venturemedia.com>
2015-10-01 12:00:03 -0700

    Initial Commit

---
+++ HDF5/HDF5.h
@@ -0,0 +1,19 @@
+//
+//  HDF5.h
+//  HDF5
+//
+//  Created by Alejandro Isaza on 2015-10-01.
+//  Copyright © 2015 Venture Media Labs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HDF5.
+FOUNDATION_EXPORT double HDF5VersionNumber;
+
+//! Project version string for HDF5.
+FOUNDATION_EXPORT const unsigned char HDF5VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HDF5/PublicHeader.h>
+
+
