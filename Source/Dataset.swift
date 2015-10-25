cf037baa3a89102487855201bb1b94fbdd2e2878
Alejandro Isaza <aisaza@venturemedia.com>
2015-10-25 09:48:35 -0700

    Update license notices

--- Source/Dataset.swift
+++ Source/Dataset.swift
@@ -1,4 +1,8 @@
-//  Copyright © 2015 Venture Media Labs. All rights reserved.
+// Copyright © 2015 Venture Media Labs. All rights reserved.
+//
+// This file is part of HDF5Kit. The full HDF5Kit copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
 
 public class Dataset {
     var id: Int32 = -1
